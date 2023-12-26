8b1e620ff1fd46c78910d78c789cc6f590452788
nuwa37 <yond41m3@icloud.com>
2023-12-26 20:59:17 +0600

    Added 1st comps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
+// import Counter from "./components/Counter";
+// import ClassCounter from "./components/ClassCounter";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+
 
 function App() {
-  const [state] = useState(0)
+
+  const [posts, setPosts] = useState ([
+    {id: 1, title: 'test', body: 'description'},
+    {id: 2, title: 'test 2', body: 'description'},
+    {id: 3, title: 'test 3', body: 'description'},
+  ])
+ 
   return (
     <div className="App">
+      <PostList posts={posts} title='Post List 1' />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PostItem = (props) => {
+    return (
+        <div className="post"> 
+        <div className="post__content"> 
+          <div className="post__content__name">
+            {props.post.id} {props.post.title}
+          </div>
+          <div className="post__content__text">
+          {props.post.body}
+          </div>
+        </div>
+        <div className="post__btns">
+            <button>delete</button>
+        </div>
+      </div>
+    );
+}
+
+export default PostItem;
